071e720a7170770a09e53b0eb3ee305c25be55e5
Daniel Haanpaa <dhaanpaa@djhent.us>
2021-05-22 13:52:21 -0700

    cleaned up code to be more functional

--- src/exp_multi_step_form/core.clj
+++ src/exp_multi_step_form/core.clj
@@ -11,31 +11,30 @@
   (read-line))
 
 (defn write-record [id step data]
-  (spit 
+  (spit
    (str "record." id "." step ".yaml")
-   (yaml/generate-string data :dumper-options {:flow-style :block})
-   )
-  )
+   (yaml/generate-string data :dumper-options {:flow-style :block})))
 
 (defn get-group-id [] (str/upper-case (str/replace (uuid/v1) "-" "")))
 
+;; Gather Data for step1 of the process
 (defn step1 [id]
   {:id id
    :first-name (prompt-for-var "First Name ..................... ")
    :last-name (prompt-for-var  "Last Name ...................... ")})
 
+;; Gather data for step2 of the process
 (defn step2 [id]
   {:id id
    :num1 (prompt-for-var "Number 1 ...................... ")
    :num2 (prompt-for-var "Number 2 ...................... ")})
 
-
-
+;; Here is an example flow
+(defn example-flow [group-id]
+  (write-record group-id "step1" (step1 group-id))
+  (write-record group-id "step2" (step2 group-id)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Welcome to my experiment"
   [& args]
-  (def record-id (get-group-id))
-  (write-record record-id "step1" (step1 record-id))
-  (write-record record-id "step2" (step2 record-id))
-  (println "Hello, World!"))
+  (example-flow (get-group-id)))
